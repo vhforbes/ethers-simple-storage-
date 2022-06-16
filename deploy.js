d86503a58274335a1a720ba0bc71a7d6ba9f00e0
Victor Forbes <vhforbes@gmail.com>
2022-06-16 17:25:55 -0300

    feat: encrypting keys with password

--- deploy.js
+++ deploy.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 const main = async () => {
   // Variables setup
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  const encryptedJson = fs.readFileSync("./encryptedKey.json", "utf8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PASSWORD);
+
+  wallet = await wallet.connect(provider);
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
 
